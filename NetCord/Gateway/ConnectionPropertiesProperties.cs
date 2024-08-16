a1c78e32406239ad1f29df08c516d52d987da155
brokiem <milyhqvideo@gmail.com>
2024-08-16 22:51:47 +0800

    `ConnectionPropertiesProperties`: Ignore JSON property if it is null

--- NetCord/Gateway/ConnectionPropertiesProperties.cs
+++ NetCord/Gateway/ConnectionPropertiesProperties.cs
@@ -20,36 +20,47 @@ public partial class ConnectionPropertiesProperties
     [JsonPropertyName("device")]
     public string? Device { get; set; }
     
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     [JsonPropertyName("release_channel")]
     public string? ReleaseChannel { get; set; }
 
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     [JsonPropertyName("client_version")]
     public string? ClientVersion { get; set; }
 
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     [JsonPropertyName("os_version")]
     public string? OsVersion { get; set; }
 
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     [JsonPropertyName("os_arch")]
     public string? OsArch { get; set; }
 
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     [JsonPropertyName("app_arch")]
     public string? AppArch { get; set; }
 
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     [JsonPropertyName("system_locale")]
     public string? SystemLocale { get; set; }
 
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     [JsonPropertyName("browser_user_agent")]
     public string? BrowserUserAgent { get; set; }
 
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     [JsonPropertyName("browser_version")]
     public string? BrowserVersion { get; set; }
 
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     [JsonPropertyName("client_build_number")]
     public int? ClientBuildNumber { get; set; }
 
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     [JsonPropertyName("native_build_number")]
     public int? NativeBuildNumber { get; set; }
 
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     [JsonPropertyName("client_event_source")]
     public string? ClientEventSource { get; set; }
 }
