4a55aed316e9d9b66e2fa9a570074e8c79b9ede8
brokiem <milyhqvideo@gmail.com>
2024-08-16 23:31:44 +0800

    `ClientStateProperties`: Fix `guild_versions` returned empty array instead of empty object

--- NetCord/Gateway/ClientStateProperties.cs
+++ NetCord/Gateway/ClientStateProperties.cs
@@ -7,7 +7,7 @@ public partial class ClientStateProperties
     public static ClientStateProperties Default => new()
     {
         ApiCodeVersion = 0,
-        GuildVersions = [],
+        GuildVersions = new { },
         HighestLastMessageId = "0",
         PrivateChannelsVersion = "0",
         ReadStateVersion = 0,
@@ -15,24 +15,31 @@ public partial class ClientStateProperties
         UserSettingsVersion = -1
     };
 
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     [JsonPropertyName("api_code_version")]
     public int ApiCodeVersion { get; set; }
 
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     [JsonPropertyName("guild_versions")]
-    public IEnumerable<object>? GuildVersions { get; set; }
+    public object? GuildVersions { get; set; }
 
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     [JsonPropertyName("highest_last_message_id")]
     public string? HighestLastMessageId { get; set; }
 
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     [JsonPropertyName("private_channels_version")]
     public string? PrivateChannelsVersion { get; set; }
 
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     [JsonPropertyName("read_state_version")]
     public int ReadStateVersion { get; set; }
 
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     [JsonPropertyName("user_guild_settings_version")]
     public int UserGuildSettingsVersion { get; set; }
 
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     [JsonPropertyName("user_settings_version")]
     public int UserSettingsVersion { get; set; }
 }
